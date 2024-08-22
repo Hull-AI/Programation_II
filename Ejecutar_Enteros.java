7e4cc6c4d6d9bf00022ba593d23b1f50f9dee22d
Hull-AI <dsrodriguezcardona@ucundinamarca.edu.co>
2024-08-22 15:03:10 -0500

    Add files via upload

---
+++ Ejecutar_Enteros.java
@@ -0,0 +1,14 @@
+package actividad_1_p2.Enteros;
+
+/**
+ *
+ * @author Hull
+ */
+public class Ejecutar_Enteros {
+
+    private static Enteros E = new Enteros();
+
+    public static void main(String[] args) {
+        E.ejecutar();
+    }
+}
