98a350834a05c055e23e698c7b0a9f38dd62bb8c
Hull-AI <dsrodriguezcardona@ucundinamarca.edu.co>
2024-08-22 23:33:28 -0500

    Update Ejecutor.java

--- Actividad_2/Operaciones/Ejecutor.java
+++ Actividad_2/Operaciones/Ejecutor.java
@@ -1,1 +1,14 @@
+package actividad_2_p2_Operaciones;
+/**
+ *
+ * @author Hull
+ */
+public class Ejecutor {
 
+    private static Operador O = new Operador();
+
+    public static void main(String[] args) {
+        O.operador();
+    }
+
+}
